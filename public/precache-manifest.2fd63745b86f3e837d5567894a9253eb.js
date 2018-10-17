b16edb0d342c9bcc1df2d95f7d4f78d86610cb1f
Cristopher Uriel <theledzed@gmail.com>
2018-10-17 14:40:21 -0500

    back

---
+++ public/precache-manifest.2fd63745b86f3e837d5567894a9253eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e9e906f8d5ad54633aa",
+    "url": "/static/js/main.0e9e906f.chunk.js"
+  },
+  {
+    "revision": "fdad143236989a4edb63",
+    "url": "/static/js/1.fdad1432.chunk.js"
+  },
+  {
+    "revision": "0e9e906f8d5ad54633aa",
+    "url": "/static/css/main.406dbe33.chunk.css"
+  },
+  {
+    "revision": "fdad143236989a4edb63",
+    "url": "/static/css/1.2a106fe7.chunk.css"
+  },
+  {
+    "revision": "ea9e7913f9d504ba6990e103a516ea92",
+    "url": "/index.html"
+  }
+];
